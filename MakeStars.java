f8498725e7139cf4294307d153964fabd93fcdfe
Elliot <ebossi10@stuy.edu>
2021-01-04 16:45:00 -0500

    fixed compiling error

--- MakeStars.java
+++ MakeStars.java
@@ -4,11 +4,11 @@ public class MakeStars{
     int length = 0;
     int i = 0;
     Scanner scan = new Scanner(System.in);
-      while (scan.hasNextLine){
+      while (scan.hasNextLine()){
         Scanner scan1 = new Scanner(scan.nextLine());
-        while (scan1.hasNext){
+        while (scan1.hasNext()){
           Scanner scan2 = new Scanner(scan1.next());
-          while (scan2.hasNext){
+          while (scan2.hasNext()){
             i = 0;
             length = scan2.next().length();
             while (i < length){
